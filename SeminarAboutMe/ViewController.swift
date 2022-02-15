3dcb84069e464cf2a589b41f77b3d1e1b019342a
Lin LaMonte <penelopelamonte@gmail.com>
2022-02-15 15:02:06 -0700

    Initial Commit

---
+++ SeminarAboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeminarAboutMe
+//
+//  Created by Lin LaMonte on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
