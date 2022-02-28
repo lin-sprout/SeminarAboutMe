7096d2b5ac079116e53269b496db6ad9272b81fa
Lin LaMonte <penelopelamonte@gmail.com>
2022-02-28 15:35:58 -0700

    Started code

--- SeminarAboutMe/ViewController.swift
+++ SeminarAboutMe/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var uiImageA: UIImageView!
+    
+    
+    @IBOutlet weak var nameLabelText: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //uiImageA.image = UIImage(named: "placehold name")
     }
 
-
+    @IBAction func uiButtonA(_ sender: Any) {
+        //uiImageA.image = UIImage(named: "placehold name2")
+    }
+    
+    
+    @IBAction func segmentControlColorChanger(_ sender: Any) {
+        
+        //        switch segementControlColorChanger.selectedSegmentIndex
+       // {
+            
+       // case 0: outlet.text = ""
+        //case 1: outlet.text = ""
+        //case 2: outlet.text = ""
+        //default: outlet.text = ""
+            //figure out how to change bg color, don't use the .text
+        //}
+    }
+    
+    
 }
 
